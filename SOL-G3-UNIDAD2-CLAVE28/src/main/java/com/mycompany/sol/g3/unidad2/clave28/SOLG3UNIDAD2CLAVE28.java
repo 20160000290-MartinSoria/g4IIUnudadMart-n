7d3127e177098c0b78a8859ea7c47d394536afe2
20160000290-MartinSoria <m.soria.5ddb@gmail.com>
2026-04-15 22:46:55 -0600

    Add files via upload

---
+++ SOL-G3-UNIDAD2-CLAVE28/src/main/java/com/mycompany/sol/g3/unidad2/clave28/SOLG3UNIDAD2CLAVE28.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sol.g3.unidad2.clave28;
+
+/**
+ *
+ * @author LAB-A--PC30
+ */
+public class SOLG3UNIDAD2CLAVE28 {
+
+    public static void main(String[] args) {
+        jfrmMensajes realMadrid = new jfrmMensajes();
+        realMadrid.setVisible(true);
+    }
+}
